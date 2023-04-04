1c36b517da8bbbe2810d1500e51f7949f2dca655
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-04-04 16:54:05 -0300

    fix: ajusta classe query para realizar requisições na api

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -17,25 +17,62 @@ export class Query {
   }
 
   order(name: string, sort: SortState) {
-    if (sort) this.query = `?page=${this.page}&max=${this.max}&${name}=${sort}`;
-    else this.defaultQuery();
+    if (!sort) return this.defaultQuery();
+
+    if (this.isDefault()) return (this.query += `&${name}=${sort}`);
+
+    this.removeQuery(name);
+    this.query = this.addQuery(`${name}=${sort}`);
 
     return this.query;
   }
 
   changePage(page: number) {
     this.page = page;
-    this.query = `?page=${this.page}&max=${this.max}`;
+
+    if (this.isDefault()) return this.defaultQuery();
+
+    this.removeQuery("page");
+    this.query = this.addQuery(`page=${page}`);
+
     return this.query;
   }
 
   changeMax(max: number) {
     this.max = max;
-    this.query = `?page=${this.page}&max=${this.max}`;
+
+    if (this.isDefault()) return this.defaultQuery();
+
+    this.removeQuery("max");
+    this.query = this.addQuery(`max=${max}`);
+
     return this.query;
+  }
+
+  private removeQuery(name: string) {
+    this.query = this.query
+      .split("&")
+      .filter((query) => !query.includes(name))
+      .join("&");
+
+    if (!this.query.includes(`?`)) this.query = `?${this.query}`;
   }
 
   private defaultQuery() {
     this.query = `?page=${this.page}&max=${this.max}`;
+    return this.query;
+  }
+
+  private isDefault() {
+    console.log(this.query.split("&"));
+    return this.query.split("&").length === 2;
+  }
+
+  private addQuery(query: string) {
+    const queryArray = this.query.split("&");
+
+    queryArray.push(query);
+
+    return queryArray.join("&");
   }
 }
