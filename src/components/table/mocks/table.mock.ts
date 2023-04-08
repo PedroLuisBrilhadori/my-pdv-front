707298f891c4d8ff0e11abc5155694f5100e4a96
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-04-08 09:41:31 -0300

    test: finaliza testes no componente table

--- src/components/table/mocks/table.mock.ts
+++ src/components/table/mocks/table.mock.ts
@@ -5,20 +5,22 @@ import { TableType } from "../types";
 export interface mockUserData {
   name: string;
   age: number;
+  salary: number;
+  active: boolean;
 }
 
 export const mockDataSource: mockUserData[] = [
-  { name: "Pedro", age: 19 },
-  { name: "Gabriela", age: 30 },
-  { name: "Karina", age: 45 },
-  { name: "Joana", age: 22 },
-  { name: "Paulo", age: 18 },
-  { name: "Marcelo", age: 49 },
-  { name: "Alcides", age: 29 },
-  { name: "Rafa", age: 10 },
-  { name: "Cecilia", age: 12 },
-  { name: "Alceu", age: 68 },
-  { name: "Carlos", age: 25 },
+  { name: "Pedro", age: 19, salary: 2000.6, active: true },
+  { name: "Gabriela", age: 30, salary: 2010.6, active: true },
+  { name: "Karina", age: 45, salary: 1000.6, active: true },
+  { name: "Joana", age: 22, salary: 3000.6, active: false },
+  { name: "Paulo", age: 18, salary: 2000.6, active: true },
+  { name: "Marcelo", age: 49, salary: 900.6, active: true },
+  { name: "Alcides", age: 29, salary: 2010.6, active: false },
+  { name: "Rafa", age: 10, salary: 3000.0, active: true },
+  { name: "Cecilia", age: 12, salary: 10, active: false },
+  { name: "Alceu", age: 68, salary: 200, active: true },
+  { name: "Carlos", age: 25, salary: 2, active: true },
 ];
 
 export const mockColumns: TableHeaderType[] = [
@@ -30,6 +32,8 @@ export const mockColumns: TableHeaderType[] = [
     sortListener: (sort) => console.log(sort),
   },
   { name: "age", displayName: "Age", type: TableColumnType.Number },
+  { name: "salary", displayName: "Salary", type: TableColumnType.Currency },
+  { name: "active", displayName: "Active", type: TableColumnType.Boolean },
 ];
 
 export const mockSearch: SearchType = {
